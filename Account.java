20fae4c37658d45adc528b2ca5ba8faeedbc35d5
Harshalak02 <109841967+Harshalak02@users.noreply.github.com>
2022-08-06 21:14:47 +0530

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,3 +1,12 @@
+/* 21ce052 Harshal Karangale
+Design a class named Account that contains:•A private int data field namedid for the account (default 0).•A private double data field named balance for the
+account (default 500₹).•A  private  double  data  field  named  annualInterestRate  that  stores  the  currentinterest rate (default 7%). Assume all accounts 
+have the same interest rate.•A  private  Date  data  field  named  dateCreated  that  stores  the  date  when  theaccount was created.•A no-arg constructor that 
+creates a default account.•A constructor that creates an account with the specified id and initial balance.•The accessor and mutator methods for id, balance, and 
+annualInterestRate.•The accessor method for dateCreated.•A method named getMonthlyInterestRate() that returns the monthlyinterest rate.•A method named 
+getMonthlyInterest() that returns the monthly interest.•A method named withdraw that withdraws a specified amount from theaccount.•A method named deposit that 
+deposits aspecified amount to the account.*/
+
 import java.util.Date;
 import java.util.Scanner;
 import java.text.SimpleDateFormat;
