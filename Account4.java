a6a2671c4501f500b15e924caba53c8ae419c70e
Harshalak02 <109841967+Harshalak02@users.noreply.github.com>
2022-08-06 22:59:32 +0530

    Update Account4.java

--- Account4.java
+++ Account4.java
@@ -1,3 +1,9 @@
+/* 21CE052 Harshal Karangale
+Subclasses  of  Account)  In  Programming  Exercise  2,  the  Account  class  wasdefined to model a bank account. An account has the properties account number,
+balance,  annual  interestrate,  and  date  created,  and  methods  to  deposit  and withdrawfunds.  Create  two  subclasses  for  checking  and  saving  accounts. 
+A checkingaccount  has  an  overdraft  limit,  but  a  savings  account  cannot  be overdrawn.Draw  the  UML  diagram  for  the  classes  and  then implement  them. 
+Writea  test  program  that  creates  objects  of  Account,  SavingsAccount,  andCheckingAccount and invokes their toString() methods.*/
+
 import java.util.Scanner;
 import java.util.Date;
 
